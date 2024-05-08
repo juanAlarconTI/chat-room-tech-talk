1e172378b100a7a48bf8fab5fd090b154eec4567
Juan Alarcon <jalarcon@arrivelogistics.com>
2024-05-08 08:18:46 -0500

    chore: Update .gitignore and add .env.local file

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
+"use client"
+import { HubConnection } from '@microsoft/signalr';
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import signalR from '@microsoft/signalr';
+import {HubConnectionBuilder}  from '@microsoft/signalr';
 
 export default function Home() {
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState<string[]>([]);
+  const [connection, setConnection] = useState<HubConnection | null>(null);
 
-  const connection = new signalR.HubConnectionBuilder().withUrl(process.env.SIGNAL_R_URL ?? '' , {accessTokenFactory: () => process.env.SIGNALR_API_KEY ?? ''}).build();
+  
 
   useEffect(() => {
-    connection.start().then(() => {
-      connection.invoke('send', 'Hello');
+    const connectionBuilder = new HubConnectionBuilder()
+    .withUrl(process.env.NEXT_PUBLIC_SIGNAL_R_URL ?? '' , {
+      accessTokenFactory: () => process.env.NEXT_PUBLIC_SIGNALR_API_KEY ?? ''
+    })
+    .build();
+    setConnection(connectionBuilder);
+    connectionBuilder.start().then(() => {
+      connectionBuilder.invoke('send', 'Hello');
 
-      connection.on('message', (message) => {
+      connectionBuilder.on('message', (message: string) => {
         setChat([...chat, message]);
       });
     });
 
     return () => {
-      connection.stop()
+      connectionBuilder.stop()
     };
   },[]);
 
@@ -29,7 +38,7 @@ export default function Home() {
   const onMessageSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
     const payload = { message };
-    connection.send('send', payload.message);
+    connection?.send('send', payload.message);
     setMessage('');
   };
 
